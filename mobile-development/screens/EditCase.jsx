d731d0cda17f73e4ad62b52aa84d6df64425b81e
Shakila <sasikaran@Sasikarans-MacBook-Pro.local>
2023-10-20 11:07:04 +0530

    Initial commit

---
+++ mobile-development/screens/EditCase.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EditCase = () => {
+  return (
+    <View>
+      <Text>EditCase</Text>
+    </View>
+  )
+}
+
+export default EditCase
+
+const styles = StyleSheet.create({})
